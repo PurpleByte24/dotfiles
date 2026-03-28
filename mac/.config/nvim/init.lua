78fcb4d5fee338e48c4138a6059d8db83e941f3b
PurpleByte24 <purplebyte24@gmail.com>
2026-03-28 15:49:02 +0100

    added some stuff to nvim in mac

--- mac/.config/nvim/init.lua
+++ mac/.config/nvim/init.lua
@@ -1,3 +1,6 @@
+-- treesitter: parsers to install
+vim.g.ts_ensure_installed = { "lua", "bash", "yaml", "json", "python", "go" }
+
 -- core options
 vim.opt.shortmess:append("I")
 vim.opt.number = true
@@ -5,6 +8,7 @@ vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.smartindent = true
 vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
@@ -14,6 +18,7 @@ vim.opt.list = true
 vim.opt.listchars = {
   space = "·",
   trail = "·",
+  tab = "→ ",
   extends = "›",
   precedes = "‹",
 }
@@ -30,6 +35,25 @@ vim.opt.splitbelow = true
 vim.opt.statusline = "%f"
 vim.opt.mouse = "a"
 vim.opt.timeoutlen = 400
+
+-- filetype overrides (ftplugins can override global settings)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "sh", "bash", "zsh", "yaml", "json", "lua", "python" },
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
+
+-- Makefiles must use real tabs
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "make",
+  callback = function()
+    vim.opt_local.expandtab = false
+  end,
+})
 
 -- disable lazy.nvim startup screen
 vim.g.lazy_nvim_disable_startup = true
@@ -48,19 +72,107 @@ vim.opt.rtp:prepend(lazypath)
 
 -- plugins
 require("lazy").setup({
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      vim.treesitter.language.register("bash", "sh")
+    end,
+  },
   { "neovim/nvim-lspconfig" },
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
   { "lukas-reineke/indent-blankline.nvim", main = "ibl" },
-  { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "L3MON4D3/LuaSnip" },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+  },
 })
 
--- plugin config
+-- indent-blankline
 require("ibl").setup()
 
--- LSP
-vim.lsp.config("lua_ls", {})
+-- nvim-cmp
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+cmp.setup({
+  snippet = {
+    expand = function(args) luasnip.lsp_expand(args.body) end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"]      = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"]     = cmp.mapping(function(fallback)
+      if cmp.visible() then cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
+      else fallback() end
+    end, { "i", "s" }),
+    ["<S-Tab>"]   = cmp.mapping(function(fallback)
+      if cmp.visible() then cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then luasnip.jump(-1)
+      else fallback() end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+  }),
+})
+
+-- shared LSP capabilities (tells servers what nvim-cmp supports)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- LSP servers
+-- requires: brew install lua-language-server
+vim.lsp.config("lua_ls", {
+  capabilities = capabilities,
+  cmd = { "lua-language-server" },
+})
 vim.lsp.enable("lua_ls")
 
+-- requires: brew install bash-language-server shellcheck shfmt
+vim.lsp.config("bashls", { capabilities = capabilities })
+vim.lsp.enable("bashls")
+
+-- requires: brew install yaml-language-server
+vim.lsp.config("yamlls", {
+  capabilities = capabilities,
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.29.0-standalone-strict/all.json"] = "/*.yaml",
+      },
+      validate = true,
+    },
+  },
+})
+vim.lsp.enable("yamlls")
+
+-- LSP keymaps (only active when LSP attaches to a buffer)
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "gd",         vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K",          vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr",         vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>f",  vim.lsp.buf.format, opts)
+    vim.keymap.set("n", "[d",         vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "]d",         vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "<leader>e",  vim.diagnostic.open_float, opts)
+  end,
+})
+
+-- Telescope keymaps
+local telescope = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", telescope.find_files)
+vim.keymap.set("n", "<leader>fg", telescope.live_grep)
+vim.keymap.set("n", "<leader>fb", telescope.buffers)
+vim.keymap.set("n", "<leader>fh", telescope.help_tags)
